3cad9e66cb49d19d34d45ecfc22b3843eca4dfe7
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2023-08-02 14:19:33 +0200

    changes to menu

--- frontend/src/app/shared/app.menu.config.ts
+++ frontend/src/app/shared/app.menu.config.ts
@@ -3,6 +3,6 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
   { id: 'registros', name: 'REGISTERS', icon: 'list', route: '/main/registros' },
+  { id: 'resumeDelivery', name: 'RESUME', route: '/main/resume-delivery', icon: 'donut_small'},
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' },
-  { id: 'resumeDelivery', name: 'RESUME', route: '/main/resume-delivery', icon: 'power_settings_new'}
 ];
