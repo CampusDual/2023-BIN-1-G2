058ae72622d622bbf0faf52851d30656c1101487
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2023-08-09 14:27:14 +0200

    1ª Historia - pendiente bug día última fecha

--- frontend/src/app/main/resume-delivery/resume-delivery-view/resume-delivery-view.component.ts
+++ frontend/src/app/main/resume-delivery/resume-delivery-view/resume-delivery-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { LineChartConfiguration } from "ontimize-web-ngx-charts";
-import { FilterExpressionUtils, Expression } from 'ontimize-web-ngx';
+import { LineChartConfiguration, DataAdapterUtils } from "ontimize-web-ngx-charts";
+import { FilterExpressionUtils, Expression } from "ontimize-web-ngx";
 import { D3LocaleService } from "src/app/shared/d3-locale/d3Locale.service";
 
 @Component({
@@ -11,43 +11,54 @@ import { D3LocaleService } from "src/app/shared/d3-locale/d3Locale.service";
 export class ResumeDeliveryViewComponent implements OnInit {
   public movementTypesChartParams: LineChartConfiguration;
 
+  public dataChart: any = [];
+
   constructor(protected d3LocaleService: D3LocaleService) {
-      const d3Locale = this.d3LocaleService.getD3LocaleConfiguration();
-      this._configureLineChart(d3Locale);
+    const d3Locale = this.d3LocaleService.getD3LocaleConfiguration();
+    this._configureLineChart(d3Locale);
   }
 
   ngOnInit() {}
 
   private _configureLineChart(locale: any): void {
     this.movementTypesChartParams = new LineChartConfiguration();
-    this.movementTypesChartParams.noDataMessage = "NO_DATA_FOUND"
-    this.movementTypesChartParams.legendPosition = "bottom"
-    this.movementTypesChartParams.legend.maxKeyLength = 23
+    this.movementTypesChartParams.noDataMessage = "NO_DATA_FOUND";
+    this.movementTypesChartParams.legendPosition = "bottom";
+    this.movementTypesChartParams.legend.maxKeyLength = 23;
   }
 
-  createFilter(values: Array<{ attr, value }>): Expression {
+  createFilter(values: Array<{ attr; value }>): Expression {
     let filters: Array<Expression> = [];
-    values.forEach(fil => {
-       if (fil.value) {
-          if (fil.attr === 'STARTDATE_I') {
-             filters.push(FilterExpressionUtils.buildExpressionMoreEqual('fecha', new Date(fil.value) ));
-          }          
-          if (fil.attr === 'ENDDATE_I') {
-             filters.push(FilterExpressionUtils.buildExpressionLessEqual('fecha', new Date(fil.value)));
-          }          
-       }
+    values.forEach((fil) => {
+      if (fil.value) {
+        if (fil.attr === "STARTDATE_I") {
+          filters.push(
+            FilterExpressionUtils.buildExpressionMoreEqual("day", fil.value)
+          );
+        }
+        if (fil.attr === "ENDDATE_I") {
+          filters.push(
+            FilterExpressionUtils.buildExpressionLessEqual("day", fil.value)
+          );
+        }
+      }
     });
-    console.log(filters);
+
     if (filters.length > 0) {
-       return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
+      return filters.reduce((exp1, exp2) =>
+        FilterExpressionUtils.buildComplexExpression(
+          exp1,
+          exp2,
+          FilterExpressionUtils.OP_AND
+        )
+      );
     } else {
-       return null;
+      return null;
     }
- }
+  }
 
-//  onFilter(data:any){
-//   console.log(data);
-//  }
-
-
+  loadData(data: Array<any>) {
+    const adapter = DataAdapterUtils.createDataAdapter(this.movementTypesChartParams)
+    this.dataChart = adapter.adaptResult(data)
+  }
 }
