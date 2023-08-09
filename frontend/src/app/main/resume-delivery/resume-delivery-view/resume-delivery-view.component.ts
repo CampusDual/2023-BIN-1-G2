02874c4ade4ce405e0b8edff6b4db001060cea45
borja <borja@example.com>
2023-08-09 12:35:49 +0200

    atascamiento filtrado fecha

--- frontend/src/app/main/resume-delivery/resume-delivery-view/resume-delivery-view.component.ts
+++ frontend/src/app/main/resume-delivery/resume-delivery-view/resume-delivery-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { LineChartConfiguration } from "ontimize-web-ngx-charts";
+import { FilterExpressionUtils, Expression } from 'ontimize-web-ngx';
 import { D3LocaleService } from "src/app/shared/d3-locale/d3Locale.service";
 
 @Component({
@@ -23,4 +24,30 @@ export class ResumeDeliveryViewComponent implements OnInit {
     this.movementTypesChartParams.legendPosition = "bottom"
     this.movementTypesChartParams.legend.maxKeyLength = 23
   }
+
+  createFilter(values: Array<{ attr, value }>): Expression {
+    let filters: Array<Expression> = [];
+    values.forEach(fil => {
+       if (fil.value) {
+          if (fil.attr === 'STARTDATE_I') {
+             filters.push(FilterExpressionUtils.buildExpressionMoreEqual('fecha', new Date(fil.value) ));
+          }          
+          if (fil.attr === 'ENDDATE_I') {
+             filters.push(FilterExpressionUtils.buildExpressionLessEqual('fecha', new Date(fil.value)));
+          }          
+       }
+    });
+    console.log(filters);
+    if (filters.length > 0) {
+       return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
+    } else {
+       return null;
+    }
+ }
+
+//  onFilter(data:any){
+//   console.log(data);
+//  }
+
+
 }
