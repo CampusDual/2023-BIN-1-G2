19e69a755511f44727b53c515719f0235dfa35f1
maurovidal23 <maurovidalantepazo@gmail.com>
2023-08-24 14:45:25 +0200

    Edición gráfica balance

--- frontend/src/app/main/resume-delivery/resume-delivery-balance/resume-delivery-balance.component.ts
+++ frontend/src/app/main/resume-delivery/resume-delivery-balance/resume-delivery-balance.component.ts
@@ -3,6 +3,7 @@ import { OTableComponent, OTranslateService } from "ontimize-web-ngx";
 import {
   DataAdapterUtils,
   DiscreteBarChartConfiguration,
+  MultiBarChartConfiguration,
   OChartComponent,
 } from "ontimize-web-ngx-charts";
 import { Subscription } from "rxjs";
@@ -17,8 +18,16 @@ import { FilterChartService } from "src/app/shared/filter-chart.service";
 })
 export class ResumeDeliveryBalanceComponent implements OnInit, OnDestroy {
   public movementTypesChartParamsBalance: DiscreteBarChartConfiguration;
+  public movementTypesChartParamsBalanceMulti: MultiBarChartConfiguration;
   public dataChartBalance: any = [];
+  public dataCharBalanceMulti:any=[];
   private translateServiceSubscription: Subscription;
+
+  @ViewChild("balanceChart", { static: true }) balanceChart: OTableComponent;
+  @ViewChild("chart", { static: true })
+  chart: OChartComponent;
+  @ViewChild("chart2", {static:true})
+  chart2:OChartComponent;
 
   constructor(
     protected d3LocaleService: D3LocaleService,
@@ -33,14 +42,16 @@ export class ResumeDeliveryBalanceComponent implements OnInit, OnDestroy {
         this.movementTypesChartParamsBalance.noDataMessage =
           translate.get("NO_DATA_FOUND");
         this.chart.updateOptions(this.movementTypesChartParamsBalance);
+        this.movementTypesChartParamsBalanceMulti.noDataMessage =
+          translate.get("NO_DATA_FOUND");
+        this.chart2.updateOptions(this.movementTypesChartParamsBalanceMulti);
       }
     );
-    this._configureLineChart(d3Locale, translate);
+    this._configureBarChart(d3Locale, translate);
+    this._configureMultiBarChart(d3Locale, translate);
   }
 
-  @ViewChild("balanceChart", { static: true }) balanceChart: OTableComponent;
-  @ViewChild("chart", { static: true })
-  chart: OChartComponent;
+  
 
   ngOnInit() {
   }
@@ -49,7 +60,7 @@ export class ResumeDeliveryBalanceComponent implements OnInit, OnDestroy {
     this.translateServiceSubscription.unsubscribe();
   }
 
-  private _configureLineChart(
+  private _configureBarChart(
     _locale: any,
     translate: OTranslateService
   ): void {
@@ -59,16 +70,32 @@ export class ResumeDeliveryBalanceComponent implements OnInit, OnDestroy {
     this.movementTypesChartParamsBalance.legend.maxKeyLength = 23;
     this.movementTypesChartParamsBalance.showLegend = true;
     this.movementTypesChartParamsBalance.legend.rightAlign = false;
-    this.movementTypesChartParamsBalance.height = 600;
     this.movementTypesChartParamsBalance.color = ["#3f51b5"];
     this.movementTypesChartParamsBalance.x1Axis.fontSize = 0;
   }
-
+  private _configureMultiBarChart(
+    _locale: any,
+    translate: OTranslateService
+  ): void {
+    this.movementTypesChartParamsBalanceMulti = new MultiBarChartConfiguration();
+    this.movementTypesChartParamsBalanceMulti.noDataMessage =
+      translate.get("NO_DATA_FOUND");
+    this.movementTypesChartParamsBalanceMulti.legend.maxKeyLength = 23;
+    this.movementTypesChartParamsBalanceMulti.showLegend = true;
+    this.movementTypesChartParamsBalanceMulti.legend.rightAlign = false;
+    this.movementTypesChartParamsBalanceMulti.color = ["#3f51b5"];
+    this.movementTypesChartParamsBalanceMulti.x1Axis.fontSize = 0;
+  }
   loadDataBalance(data: Array<any>) {
+    console.log(data)
     const adapter = DataAdapterUtils.createDataAdapter(
       this.movementTypesChartParamsBalance
     );
+    const adapter2= DataAdapterUtils.createDataAdapter(
+      this.movementTypesChartParamsBalanceMulti
+    )
 
     this.dataChartBalance = adapter.adaptResult(data);
+    this.dataCharBalanceMulti=adapter2.adaptResult(data);
   }
 }
