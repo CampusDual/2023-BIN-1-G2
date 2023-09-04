cc53ff154aa0b9a2085c7fa68be111ff84f78a8e
maurovidal23 <maurovidalantepazo@gmail.com>
2023-09-04 15:06:47 +0200

    Habilitando botones en el detalle de pedidos

--- frontend/src/app/main/order/order-detail/order-detail.component.ts
+++ frontend/src/app/main/order/order-detail/order-detail.component.ts
@@ -1,25 +1,44 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
 import {
   Expression,
   FilterExpressionUtils,
+  OFormComponent,
   OTableComponent,
 } from "ontimize-web-ngx";
+
 
 @Component({
   selector: "app-order-detail",
   templateUrl: "./order-detail.component.html",
   styleUrls: ["./order-detail.component.css"],
 })
-export class OrderDetailComponent implements OnInit {
-
+export class OrderDetailComponent implements OnInit, AfterViewInit {
+  @ViewChild("formDetail",{static:true}) formDetail: OFormComponent
+  
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+  
+  }
+
+  ngAfterViewInit(){
+    console.log(this.formDetail.getFieldValue("id_order"))
+    if(this.formDetail.getFieldValue("id_order")==="new"){
+    console.log(this.formDetail.getFieldValue("id_order"))
+    this.formDetail.setFieldValue("id_order",-1)
+    }
+  }
+
+  changeNew(data:any){
+    if (data.id_order==="new")
+    this.formDetail.setFieldValue("id_order", -1)
+  }
 
   createFilter(values: Array<{ attr; value }>): Expression {
-
+    
     const filter = values[0];
-    if (filter === undefined || filter.value === undefined) return null;
+    if (filter === undefined || filter.value === undefined|| filter.value==="new")
+      return FilterExpressionUtils.buildExpressionEquals("id_order", -1);
 
     return FilterExpressionUtils.buildExpressionEquals(
       "id_order",
