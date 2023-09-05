4aa5d2bc944ecc99a687dd84972914d34bb4a3b4
maurovidal23 <maurovidalantepazo@gmail.com>
2023-09-05 09:49:01 +0200

    Menú pedidos con todas las funcionalidades implementadas

--- frontend/src/app/main/order/order-detail/order-detail.component.ts
+++ frontend/src/app/main/order/order-detail/order-detail.component.ts
@@ -2,47 +2,50 @@ import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
 import {
   Expression,
   FilterExpressionUtils,
+  OCurrencyInputComponent,
   OFormComponent,
   OTableComponent,
 } from "ontimize-web-ngx";
-
 
 @Component({
   selector: "app-order-detail",
   templateUrl: "./order-detail.component.html",
   styleUrls: ["./order-detail.component.css"],
 })
-export class OrderDetailComponent implements OnInit, AfterViewInit {
-  @ViewChild("formDetail",{static:true}) formDetail: OFormComponent
-  
+export class OrderDetailComponent implements OnInit {
+  @ViewChild("formDetail", { static: true }) formDetail: OFormComponent;
+  @ViewChild("priceVol", { static: true }) priceVol: OCurrencyInputComponent;
+  @ViewChild("deliveryTable", { static: true }) deliveryTable: OTableComponent;
+
   constructor() {}
 
-  ngOnInit() {
-  
-  }
+  ngOnInit() {}
 
-  ngAfterViewInit(){
-    console.log(this.formDetail.getFieldValue("id_order"))
-    if(this.formDetail.getFieldValue("id_order")==="new"){
-    console.log(this.formDetail.getFieldValue("id_order"))
-    this.formDetail.setFieldValue("id_order",-1)
-    }
-  }
-
-  changeNew(data:any){
-    if (data.id_order==="new")
-    this.formDetail.setFieldValue("id_order", -1)
+  changeNew(data: any) {
+    if (data.id_order === "new") this.formDetail.setFieldValue("id_order", -1);
+    this.deliveryTable.refresh()
   }
 
   createFilter(values: Array<{ attr; value }>): Expression {
-    
     const filter = values[0];
-    if (filter === undefined || filter.value === undefined|| filter.value==="new")
+    if (
+      filter === undefined ||
+      filter.value === undefined ||
+      filter.value === "new"
+    )
       return FilterExpressionUtils.buildExpressionEquals("id_order", -1);
 
     return FilterExpressionUtils.buildExpressionEquals(
       "id_order",
       Number(filter.value)
     );
+  }
+
+  priceFunction(value: any[]) {
+    if (this.formDetail === undefined) return 0;
+    console.log(value)
+    return value
+      .filter((a) => a.calculated_volume !== undefined)
+      .reduce((acc, newSumando) => acc + newSumando.calculated_volume,0);
   }
 }
